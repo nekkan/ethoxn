fe12ab51a466e84c8a9052a314063d0da9400f2a
nekkan <nxkkdev@gmail.com>
2021-01-12 15:34:14 -0300

    style: Move extensions.kt to shared sources

--- grammar/jvm/test/com/nekkan/grammar/extensions.kt
+++
@@ -1,7 +0,0 @@
-@file:JvmName("Extensions")
-
-package com.nekkan.grammar
-
-import org.antlr.v4.kotlinruntime.CharStreams
-
-inline fun input(value: String) = CharStreams.fromString(value)
---
+++ grammar/shared/sources/com/nekkan/grammar/extensions.kt
@@ -0,0 +1,13 @@
+@file:Suppress("nothing_to_inline")
+
+package com.nekkan.grammar
+
+import org.antlr.v4.kotlinruntime.CharStream
+import org.antlr.v4.kotlinruntime.CharStreams
+import org.antlr.v4.kotlinruntime.CommonTokenStream
+
+inline fun input(value: String) = CharStreams.fromString(value)
+
+inline fun lexer(input: CharStream) = EthoxnLexer(input)
+
+inline fun parser(lexer: EthoxnLexer) = EthoxnParser(CommonTokenStream(lexer))
